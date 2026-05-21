0d05db3248c3e5ef83a3b78da28c20990ee7ac62
lisa <lisa.martin.lm02@gmail.com>
2026-05-21 09:20:44 +0200

    fix: rotation cube and file splitting

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
 from ursina import *
+import scene_cube
 
 app = Ursina()
-cube_group = Entity()
 
-
-cubes = []
-for z in range(3):
-    for y in range(3):
-        for x in range(3):
-            cube = Entity(parent=cube_group, 
-                        model='cube',
-                        color=color.red,
-                        texture="white_cube", 
-                        scale=(1), position=(x - 1, y - 1, z - 1)
-                    )
-            cubes.append(cube)
-
-dragging = False
-last_mouse_pos = Vec2(0, 0)
-
+scene_cube.create_cube()
 
 def input(key):
-    global dragging, last_mouse_pos
-
-    if key == 'left mouse down':
-        dragging = True
-        last_mouse_pos = mouse.position
-
-    if key == 'left mouse up':
-        dragging = False
-
+    scene_cube.input(key)
 
 def update():
-    global last_mouse_pos
-
-    if dragging:
-        delta = mouse.position - last_mouse_pos
-
-        # rotation selon mouvement souris
-        cube_group.rotation_y += delta.x * 200
-        cube_group.rotation_x -= delta.y * 200
-
-        last_mouse_pos = mouse.position
+    scene_cube.update()
 
 
 app.run()
